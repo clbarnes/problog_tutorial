04920bf4b040a4fe4fc61d47ceff0bb47d07f04d
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-02-24 15:29:39 -0500

    improved tests to split out error cases

--- tests.py
+++ tests.py
@@ -9,16 +9,29 @@ EDITED_DIR = 'edited'
 LONG_RUNNING = []  # fill in if any are really slow
 
 
-def test_pl_files():
+def generate_pl_files():
     for tutorial_name in os.listdir(ORIGINALS_DIR):
         for filename in os.listdir(os.path.join(ORIGINALS_DIR, tutorial_name)):
-            if filename.endswith('.pl'):
-                if os.path.join(tutorial_name, filename) not in LONG_RUNNING:
-                    yield (
-                        check_results,
-                        os.path.join(ORIGINALS_DIR, tutorial_name, filename),
-                        os.path.join(EDITED_DIR, tutorial_name, filename)
-                    )
+            if filename.endswith('.pl') and os.path.join(tutorial_name, filename) not in LONG_RUNNING:
+                yield (
+                    os.path.join(ORIGINALS_DIR, tutorial_name, filename),
+                    os.path.join(EDITED_DIR, tutorial_name, filename)
+                )
+
+
+def test_exist():
+    for original_path, edited_path in generate_pl_files():
+        yield check_exists, original_path, edited_path
+
+
+def test_edited():
+    for original_path, edited_path in generate_pl_files():
+        yield check_edited, original_path, edited_path
+
+
+def test_same_results():
+    for original_path, edited_path in generate_pl_files():
+        yield check_results, original_path, edited_path
 
 
 def run_pl_file(path):
@@ -28,12 +41,27 @@ def run_pl_file(path):
     return ProbLog.convert(PrologString(s), SDD).evaluate()
 
 
-def check_results(original_path, edited_path):
+def check_exists(original_path, edited_path):
     assert os.path.exists(original_path), 'Original file path {} does not exist'.format(original_path)
     assert os.path.exists(edited_path), 'Edited file path {} does not exist'.format(edited_path)
 
+
+def check_edited(original_path, edited_path):
+    strings = []
+
+    for path in [original_path, edited_path]:
+        with open(path) as f:
+            strings.append(f.read())
+
+    assert strings[0] != strings[1], 'File {} has not been edited'.format(edited_path)
+
+
+def check_results(original_path, edited_path):
     original_results = run_pl_file(original_path)
     edited_results = run_pl_file(edited_path)
 
     assert original_results == edited_results, \
         'Result from {} does not match result from {}'.format(edited_path, original_path)
+
+
+
